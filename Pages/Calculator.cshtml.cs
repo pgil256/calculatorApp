04c7e1ce5158bc15874abcd5354ffb1be998fa29
Patrick Gilhooley <113308245+pgil256@users.noreply.github.com>
2024-04-09 16:15:22 -0400

    Add files via upload

---
+++ Pages/Calculator.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyCalculatorApp.Pages
+{
+    public class CalculatorModel : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
